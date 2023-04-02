31b36df9a8d952a8028a74cbd8ff248b4089486b
Allas Assis <allasassis10@gmail.com>
2023-04-02 19:17:06 -0300

    Tratador de erros adicionado

---
+++ src/main/java/spring/bank/api_bank/infra/exception/TratadorDeErros.java
@@ -0,0 +1,39 @@
+package spring.bank.api_bank.infra.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@RestControllerAdvice
+public class TratadorDeErros {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarErro404() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException e) {
+        List<FieldError> errors = e.getFieldErrors();
+
+        return ResponseEntity.badRequest().body(errors.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    @ExceptionHandler(ValidacaoException.class)
+    public ResponseEntity tratarErroValidacao(ValidacaoException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    private record DadosErroValidacao(String campo, String mensagem) {
+
+        public DadosErroValidacao(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+}
+
